a6d129150edd94c532e28f6e28e37f8dafc64d85
MateaLukiccc <ml20200410@student.fon.bg.ac.rs>
2022-10-25 12:55:06 +0200

    rearranged online project

--- online/client.py
+++ online/client.py
@@ -1,59 +1,27 @@
 import pygame
 from network import Network
+from pygame import Player
 
-width = 500
-height = 500
-win = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Client")
+WIDTH = 500
+HEIGHT = 500
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("client")
 
 clientNumber = 0
 
 
-class Player():
-    def __init__(self, x, y, width, height, color):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-        self.rect = (x,y,width,height)
-        self.vel = 3
-
-    def draw(self, win):
-        pygame.draw.rect(win, self.color, self.rect)
-
-    def move(self):
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_LEFT]:
-            self.x -= self.vel
-
-        if keys[pygame.K_RIGHT]:
-            self.x += self.vel
-
-        if keys[pygame.K_UP]:
-            self.y -= self.vel
-
-        if keys[pygame.K_DOWN]:
-            self.y += self.vel
-
-        self.update()
-
-    def update(self):
-        self.rect = (self.x, self.y, self.width, self.height)
-
-
 def read_pos(str):
+    # we get position like 45,167
     str = str.split(",")
     return int(str[0]), int(str[1])
 
 
 def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
+    return str(tup[0]) + ',' + str(tup[1])
 
 
-def redrawWindow(win,player, player2):
-    win.fill((255,255,255))
+def redrawWindow(win, player, player2):
+    win.fill((255, 255, 255))
     player.draw(win)
     player2.draw(win)
     pygame.display.update()
@@ -62,11 +30,11 @@ def redrawWindow(win,player, player2):
 def main():
     run = True
     n = Network()
+    # when we connect we want to return to client their starting position
     startPos = read_pos(n.getPos())
-    p = Player(startPos[0],startPos[1],100,100,(0,255,0))
-    p2 = Player(0,0,100,100,(255,0,0))
+    p = Player(startPos[0], startPos[1], 100, 100, (0, 255, 0))
+    p2 = Player(0, 0, 100, 100, (255, 0, 0))
     clock = pygame.time.Clock()
-
     while run:
         clock.tick(60)
         p2Pos = read_pos(n.send(make_pos((p.x, p.y))))
@@ -78,8 +46,8 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
-
         p.move()
         redrawWindow(win, p, p2)
+
 
 main()
--- online/network.py
+++ online/network.py
@@ -4,7 +4,8 @@ import socket
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "192.168.1.102"
+        # server address
+        self.server = "192.168.46.1"
         self.port = 5555
         self.addr = (self.server, self.port)
         self.pos = self.connect()
@@ -16,6 +17,7 @@ class Network:
         try:
             self.client.connect(self.addr)
             return self.client.recv(2048).decode()
+
         except:
             pass
 
@@ -25,3 +27,7 @@ class Network:
             return self.client.recv(2048).decode()
         except socket.error as e:
             print(e)
+
+
+
+
